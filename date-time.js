46031ce0ff5e4f112f949e592174e7a92221f59b
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-04-16 23:49:25 +0100

    successfully implemented transfers

---
+++ date-time.js
@@ -0,0 +1,45 @@
+/**
+ * Returns the current date as a string in the format DD-MM-YYYY
+ * @author Obumuneme Nwabude
+ * @return {string} The current date in the format DD-MM-YYYY
+ */
+exports.getDate = () => {
+  // get the current date 
+  let dates = (new Intl.DateTimeFormat('en-GB')).format(new Date()).split('/');
+
+  // ensure that the day and month are two digits long
+  if (dates[0].length === 1) dates[0] = '0' + dates[0];
+  if (dates[1].length === 1) dates[1] = '0' + dates[1];
+
+  // rearrange the date order from MM-DD to DD-MM
+  const hold = dates[1];
+  dates[1] = dates[0];
+  dates[0] = hold;
+
+  // return the date, formatted as required
+  return dates.join('-');
+};
+
+/**
+ * Returns the current time as a string in the format HH:MMam/pm
+ * @author Obumuneme Nwabude
+ * @return {string} The current time in the format HH:MMam/pm
+ */
+exports.getTime = () => {
+  // get the current time 
+  const times = (new Intl.DateTimeFormat('en', {
+    hour:'numeric', 
+    minute:'numeric'
+  })).format(new Date()).split(' ');
+
+  // modify the am pm portion of the timeString
+  times[1] = times[1].toLowerCase();
+
+  // ensure the hour is two digits long
+  const times1 = times[0].split(':');
+  if (times1[0].length === 1) times1[0] = '0' + times1[0];
+  times[0] = times1.join(':');
+
+  // return the time, formatted as required
+  return times.join('');
+};
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const dateTime = require('./date-time');
 const User = require('./models/user');
 const Logger = require('./models/logger');
 const auth = require('./middleware/auth');
@@ -90,7 +91,7 @@ app.post('/createUser', (req, res) => {
             phoneNumber: req.body.phoneNumber,
             password: hashedPassword,
             amount: req.body.amount,
-            transactionLogs: req.body.transactionLogs || '',
+            transactionLogs: req.body.transactionLogs || [],
             transactionPin: hashedTransactionPin
           });
           // save and return the user
@@ -274,14 +275,40 @@ app.post('/transferFunds/:id', auth, transfer, (req, res) => {
   sender.amount = sender.amount - Number(req.body.amount);
   // credit the receiver
   receiver.amount = receiver.amount + Number(req.body.amount);
-  // return
-  res.status(201).json({
-    message: 'Transfer Successful',
-    amount: Number(req.body.amount),
-    senderId: req.params.id,
-    senderPhoneNumber: sender.phoneNumber,
-    receiverPhoneNumber: receiver.phoneNumber
-  });
+
+  // CREATE TRANSFER DETAILS 
+  // create detail for sender
+  const senderDetail = `DEBIT senderPhoneNumber(me):${sender.phoneNumber} ` + 
+    `receiverPhoneNumber:${receiver.phoneNumber} amount:${Number(req.body.amount)} ` + 
+    `date:${dateTime.getDate()} time:${dateTime.getTime()}`;
+  // add to its transactionLogs
+  sender.transactionLogs.push(senderDetail);
+  // create detail for receiver
+  const receiverDetail = `CREDIT senderPhoneNumber:${sender.phoneNumber} ` + 
+    `receiverPhoneNumber(me):${receiver.phoneNumber} amount:${Number(req.body.amount)} ` + 
+    `date:${dateTime.getDate()} time:${dateTime.getTime()}`;
+  // add to its transactionLogs
+  receiver.transactionLogs.push(receiverDetail);
+
+  // save sender and receiver changes and return
+  sender.save().then(updatedSender => {
+    receiver.save().then(updatedReceiver => {
+      // return
+      return res.status(201).json({
+        message: 'Transfer Successful',
+        amount: Number(req.body.amount),
+        senderId: req.params.id,
+        senderPhoneNumber: updatedSender.phoneNumber,
+        receiverPhoneNumber: updatedReceiver.phoneNumber
+      });
+    }).catch(err => res.status(500).json({
+      message: 'Error occured at saving sender',
+      error: err
+    }));
+  }).catch(err => res.status(500).json({
+    message: 'Error occured at saving sender',
+    error: err
+  }));
 });
 
 module.exports = app.listen(port);
